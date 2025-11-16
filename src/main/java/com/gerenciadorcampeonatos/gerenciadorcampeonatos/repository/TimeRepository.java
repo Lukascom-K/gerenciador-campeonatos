89f76c19da7d3561929cef1f317f178c406c16cf
Lukas <lukadmello04@gmail.com>
2025-11-16 17:49:14 -0300

    feat: Correcao de pacotes, atualizacao de entidade e entrega final com documentacao

--- src/main/java/com/gerenciadorcampeonatos/gerenciadorcampeonatos/repository/TimeRepository.java
+++ src/main/java/com/gerenciadorcampeonatos/gerenciadorcampeonatos/repository/TimeRepository.java
@@ -2,7 +2,8 @@
 
 package com.gerenciadorcampeonatos.repository;
 
-import com.gerenciadorcampeonatos.gerenciadorcampeonatos.model.Time;
+// CORRIGIDO: Removida a duplicação "gerenciadorcampeonatos" do import
+import com.gerenciadorcampeonatos.model.Time;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
